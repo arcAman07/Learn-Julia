e5a85d7db451de2e3f94f637d3701d39c5db87a7
Aman Sharma <76823502+arcAman07@users.noreply.github.com>
2022-01-28 02:01:47 +0530

    Add files via upload

---
+++ Strings/Strings.jl
@@ -0,0 +1,40 @@
+s = "I am Aman"
+
+# Another way of representing a string
+
+s2 = """I am a God"""
+
+"Here, we get an "error" because it's ambiguous where this string ends "
+
+"""Look, Mom, no "errors"!!! """
+
+typeof('a')
+
+'We will get an error here'
+
+name = "Aman"
+age = 19
+fav_number = 7
+
+println("My name is $name, $age years old and having $fav_number as my favourite number")
+
+s3 = "I am Aman "
+s4 = "and I am a God"
+num_no = 2
+
+string(s3,s4)
+
+string("I dont know ",num_no," but its greater than 1")
+
+s3 * s4
+
+a = 3
+b = 4
+c = string(a," + ",b)
+d = string(a+b)
+
+c
+
+d
+
+
